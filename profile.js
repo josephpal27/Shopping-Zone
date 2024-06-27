011a6e5ba07bae01b0b3d9dbf92bdfe47e233a99
Joseph Pal <josephpal27@gmail.com>
2024-06-28 02:41:15 +0530

    profile page updated

---
+++ profile.js
@@ -0,0 +1,34 @@
+
+    let editBtn = document.querySelectorAll('.edit-btn');
+    let saveBtn = document.querySelectorAll('.save-btn');
+
+
+    editBtn.forEach((editButton) => {
+       editButton.addEventListener('click', (event) => {
+      
+        let inputField = event.target.closest('div').querySelector('input');
+        inputField.removeAttribute('readonly');
+        inputField.style.backgroundColor = 'white';
+        inputField.focus();
+        let value = inputField.value;
+        inputField.setSelectionRange(value.length, value.length);
+
+        let saveButton = event.target.closest('div').querySelector('.save-btn');
+        saveButton.style.display = 'inline-block';
+      });
+    });
+  
+ 
+
+    saveBtn.forEach((saveButton) => {
+      saveButton.addEventListener('click', (event) => {
+
+        let inputField = event.target.closest('div').querySelector('input');
+        inputField.setAttribute('readonly', true);
+        inputField.style.backgroundColor = 'rgb(243, 243, 243)';
+
+        let editButton = event.target.closest('div').querySelector('.edit-btn');
+        editButton.style.display = 'inline-block';
+      });
+    });
+  
