a9afab5aa0c969804b4b491e39254c9630e8c806
Joseph Pal <josephpal27@gmail.com>
2024-07-12 21:34:30 +0530

    profile

--- profile.php
+++ profile.php
@@ -113,49 +113,37 @@
 
           <!-- Right Side -->
           <main>
-<<<<<<< HEAD:profile.html
-            <form action="" method="post">
-=======
           <form action="profile.back.php" method="post">
->>>>>>> 6faba02484261709e6985fee411cde6e27c07c75:profile.php
             <div class="box">
                 <h5>Profile Information</h5>
                 <div>
-                  <!-- <form action="profile.back.php" method="post"> -->
                     <input type="text" value="<?php echo isset($_SESSION['user']) ? ucwords($_SESSION['user']) : 'User' ?>" name="username" readonly>
                     <button class="edit-btn" type="button">Edit</button>
                     <button class="save-btn" type="submit">Save</button>
-                  <!-- </form>   -->
                 </div>
             </div>
             <div class="box">
                 <h5>Email Address</h5>
                 <div>
-                  <!-- <form action="profile.back.php" method="post"> -->
                     <input type="email" value="user@gmail.com" name="email" readonly>
                     <button class="edit-btn" type="button">Edit</button>
                     <button class="save-btn" type="submit">Save</button>
-                  <!-- </form> -->
                 </div>
             </div>
             <div class="box">
                 <h5>Phone Number</h5>
                 <div>
-                  <!-- <form action="profile.back.php" method="post"> -->
                     <input type="number" value="1234567890" name="phone" readonly>
                     <button class="edit-btn" type="button">Edit</button>
                     <button class="save-btn" type="submit">Save</button>
-                  <!-- </form> -->
                 </div>
             </div>
             <div class="box">
                 <h5>Delivery Address</h5>
                 <div>
-                  <!-- <form action="profile.back.php" method="post"> -->
                     <input type="text" value="Kolkata" name="address" readonly>
                     <button class="edit-btn" type="button">Edit</button>
                     <button class="save-btn" type="submit">Save</button>
-                  <!-- </form> -->
                 </div>
             </div>
           </form>
